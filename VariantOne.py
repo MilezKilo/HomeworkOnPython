baed96e6ab78012a92ed40e081cf0a0ee4f23b5a
MaksPonizov <76489164+MilezKilo@users.noreply.github.com>
2021-12-02 21:33:46 +0300

    Some changes in decoder

--- VariantOne.py
+++ VariantOne.py
@@ -3,11 +3,16 @@ import time
 
 #Декоратор
 def myDecorator(myFunction):
-        print('Вызвана функция: {}'.format(myFunction.__name__)) #Выводит в консоль название вызванной функции
+    def wrapperFunc():
+        startTime = time.time()
+        print('Вызвана функция: {}'.format(myFunction.__name__),'.') #Выводит в консоль название вызванной функции
         myFunction() #Вызов функции
-        print('затраченное время: %s' % time.perf_counter()) #Выводит в консоль затраченное время
+        endTime = time.time()
+        print('Затраченное время: %s секунды' % (endTime - startTime)) #Выводит в консоль затраченное время
+    return wrapperFunc
 
 #Найти площадь участка. Создайте программу, запрашиваемого у пользователя длину и ширину садового участка в футах. Выведите на экран площадь участка в акрах
+@myDecorator
 def areaCalculation():
     print('Введите длину и ширину:')
     lenght = int(input())
@@ -16,11 +21,13 @@ def areaCalculation():
     SQFT_PER_ACRE = temporary / 43560
     print("Результат =", SQFT_PER_ACRE, "акров")
 
+@myDecorator
 def freeFall(accseleration = 9.8):
     print('Введите дистанцию:')
     distance = int(input())
     Vf = math.sqrt(2 * (accseleration * distance))
     print('Скорость объекта при его соприкосновении с землей: ', Vf)
-    
-myDecorator(areaCalculation)
-myDecorator(freeFall)
+
+areaCalculation()
+print('----------------------------')
+freeFall()
